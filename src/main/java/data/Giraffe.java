310d4f844f1c4dcb2d5a03fc939d795e3ed6250d
Christian <chrbudtz@gmail.com>
2021-11-17 14:29:53 +0100

    Initial commit

---
+++ src/main/java/data/Giraffe.java
@@ -0,0 +1,25 @@
+package data;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Giraffe {
+    private String name;
+    private int serialNo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSerialNo() {
+        return serialNo;
+    }
+
+    public void setSerialNo(int serialNo) {
+        this.serialNo = serialNo;
+    }
+}
